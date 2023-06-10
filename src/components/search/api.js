6a959db4ecb86ddc4aae938eaf7de714a30ca9f5
Mayuresh Veeramallu <veeramallumayuresh@gmail.com>
2023-06-10 19:59:59 +0530

    Weather App made by me

---
+++ src/components/search/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c73b42667amshb6ba0e87155376bp1fba3ejsn7b54ca6ebeb5", // enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = ""; // enter your key from openweather API
